f05805572937199c4cec8b2af85b93bfc188d30d
Matteo Visconti di Oleggio Castello <mvdoc@users.noreply.github.com>
2023-11-30 14:40:35 -0800

    FIX Make loading of GMM (more) future proof, improve dockerfile (#12)
    * NF load GMM model by loading parameters
    
    * MNT run ruff, use packaging.version
    
    * MNT drop sklearn version requirements
    
    * FIX dockerfile
    
    * NF use more recent ubuntu, download blender from website
    
    * FIX install dependencies, pin pymeshlab due to issues
    
    * MNT pin pymeshlab due to issues with recent versions
    
    * ENH be verbose when applying meshlab filters
    
    * FIX scipy lib error

--- generate_dockerfile.sh
+++ generate_dockerfile.sh
@@ -1,20 +1,30 @@
+#!/bin/bash 
 # Use neurodocker to generate a dockerfile
 docker run --rm repronim/neurodocker:latest generate docker \
 	--pkg-manager apt \
-	--base ubuntu:bionic \
-	--install libopengl0 build-essential meshlab blender=2.79.b+dfsg0-1ubuntu1.18.04.1 \
+	--base-image ubuntu:20.04 \
+	--env "DEBIAN_FRONTEND=noninteractive" \
+	--run "chmod 777 /tmp" \
+	--install git libopengl0 build-essential curl ca-certificates libxi6 libglu1-mesa libglib2.0-0 libxrender1 \
+	--run "mkdir -p /opt/blender
+	curl -fL https://download.blender.org/release/Blender2.79/blender-2.79b-linux-glibc219-x86_64.tar.bz2 | tar -xj -C /opt/blender --strip-components 1" \
+	--env "PATH=/opt/blender:\$PATH" \
+	--run "blender --version" \
+	--run "mkdir -p /opt/meshlab
+	curl -fL https://github.com/cnr-isti-vclab/meshlab/releases/download/MeshLab-2022.02/MeshLab2022.02d-linux.tar.gz | tar -xz -C /opt/meshlab" \
+	--env "PATH=/opt/meshlab/usr/bin:\$PATH" \
+	--run "meshlab --version" \
 	--env SKLEARN_NO_OPENMP=1 \
 	--copy . "/headcase-pipeline" \
 	--miniconda \
-	use_env=base \
-	activate=True \
-	conda_install="python=3 numpy scipy cython" \
+	version=latest \
+	conda_install="python=3.9 gxx_linux-64" \
 	pip_install="-r /headcase-pipeline/requirements.txt" \
-	--run "pip install --upgrade numpy" \
 	--run "useradd -m -s /bin/bash -G users headcase" \
 	--env HOME=/home/headcase \
 	--run "mkdir /home/headcase/.config" \
-	--run "python -c 'import cortex'" \
+	--run "python -c 'import cortex; print(cortex.__version__)'" \
+	--run "python -c 'import pymeshlab'" \
 	--workdir "/headcase-pipeline" \
 	--entrypoint "python /headcase-pipeline/make_headcase.py" \
 	> Dockerfile
